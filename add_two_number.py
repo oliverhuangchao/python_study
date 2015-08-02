b7cab34f2b6b0a1da28e35623fe7d32917f2cc4b
oliverhuangchao <chaoh@g.clemson.edu>
2015-08-02 15:05:53 -0400

    add bit file

--- add_two_number.py
+++ add_two_number.py
@@ -1,48 +1,49 @@
-# achieve two number add with different base
-# N should <= 10, then use str() is ok
-# if N > 10:
-# there should be a new function to achieve, or use hex()
-# suppose len(a) >= len(b)
+# add two number just use bit manipulation
+import pdb
+def aplusb(a,b):
+    # if b == 0:
+    #     return a
+    # x = a ^ b # sum without carry
+    # y = (a & b) << 1 # only carry, but move 1 bit right
+    # return aplusb(x,y)
+    while b!=0:
+        carry = a&b
+        a = a^b
+        b = carry<<1
+    return a
+
+# check whether a number is a positive or not
+def isPositive(a):
+    flag = (a & 0x8000) >> 1
+    if flag > 1:
+        return False
+    else:
+        return True
+# if it is a negetive, change to -a
+def turnPositive(a):
+    if not isPositive(a):
+        return aplusb((~a),1)
+    else:
+        return a
+
+# decide which value is bigger
+def compare(a,b):
+    tmp = 0x4000
+    while tmp > 0:
+        x = tmp & a
+        y = tmp & b
+        if x == y:
+            tmp = tmp >> 1
+        else:
+            if x > y:
+                return 1
+            else: 
+                return -1
+    return 0
 
 
-def transfer(tmp):
-	if tmp < 10:
-		return str(tmp)
-	else:
-		return list(hex(tmp))[2].upper()
 
-
-def addTwo(a,b,N):
-	if len(a) < len(b):
-		return addTwo(b,a,N)
-	list_x = list(a)
-	list_x.reverse()
-	list_y = list(b)
-	list_y.reverse()
-	flag = 0
-	res = list()
-	#size = len(list_y)
-	for i in range(len(list_x)):
-		if i < len(list_y):
-			tmp  = int(list_x[i]) + int(list_y[i]) + flag
-		else:
-			tmp = int(list_x[i]) + flag
-		if tmp >= N:
-			flag = 1
-
-		tmp %= N
-		res.append(transfer(tmp))
-
-	if flag == 1:
-		res.append(transfer(flag))
-	res.reverse()
-	res = "".join(res)
-	return res
-
-
-a = "111"
-b = "999"
-N = 16
-print a
-print b
-print addTwo(a,b,N)
+a = 3
+b = -2
+print "a is %d, b is %d" % (a,b)
+print aplusb(a,b)
--- test.py
+++ test.py
@@ -1,36 +1,49 @@
-# reverse list
+# add two number just use bit manipulation
+import pdb
+def aplusb(a,b):
+    # if b == 0:
+    #     return a
+    # x = a ^ b # sum without carry
+    # y = (a & b) << 1 # only carry, but move 1 bit right
+    # return aplusb(x,y)
+    while b!=0:
+        carry = a&b
+        a = a^b
+        b = carry<<1
+    return a
 
-class listnode():
-    def __init__(self,x):
-        self._value = x
-        self._next = None
-
-def revereList(head):
-    return rev(head,None)
-
-
-def rev(head,newhead):
-    if head is None:
-        return newhead
+# check whether a number is a positive or not
+def isPositive(a):
+    flag = (a & 0x8000) >> 1
+    if flag > 1:
+        return False
     else:
-        next = head._next
-        head._next = newhead
-        return rev(next,head)
+        return True
+# if it is a negetive, change to -a
+def turnPositive(a):
+    if not isPositive(a):
+        return aplusb((~a),1)
+    else:
+        return a
+
+# decide which value is bigger
+def compare(a,b):
+    tmp = 0x4000
+    while tmp > 0:
+        x = tmp & a
+        y = tmp & b
+        if x == y:
+            tmp = tmp >> 1
+        else:
+            if x > y:
+                return 1
+            else: 
+                return -1
+    return 0
 
 
-def printlist(head):
-    while head is not None:
-        print head._value
-        head = head._next
 
-
-x = map(lambda x:listnode(x),range(5))
-head = x[0]
-start = head
-for i in range(1,len(x)):
-    x[i-1]._next = x[i]
-    #i = head
-
-printlist(head)
-head = revereList(head)
-printlist(head)
+a = 3
+b = -2
+print "a is %d, b is %d" % (a,b)
+print aplusb(a,b)
