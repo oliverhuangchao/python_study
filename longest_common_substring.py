61ddc9136d4ddce1de5991bec1878cb905dda495
oliverhuangchao <chaoh@g.clemson.edu>
2015-08-04 02:25:18 -0400

    add longest common substring

--- longest_common_substring.py
+++ longest_common_substring.py
@@ -7,20 +7,19 @@ def longestCommonSubstring(a, b):
     # pay attention the order of a and b
     check = [[0 for i in range(len(b)+1)] for j in range(len(a)+1)]
     res = 0
-    
     for i in range(1,len(a)+1):
         for j in range(1,len(b)+1):
             if a[i-1] == b[j-1]:
                 check[i][j] = check[i-1][j-1] + 1
             else:
                 check[i][j] = 0
-            
             res = max(res,check[i][j])
-    
     return res
 
 def longestCommonSubsequence(a, b):
     # write your code here
+    # subsequence may not be continue with each char
+    # that is the difference with the common substring
     check = [[0 for i in range(len(b)+1)] for j in range(len(a)+1)]
     res = 0
     for i in range(1,len(a)+1):
@@ -29,9 +28,7 @@ def longestCommonSubsequence(a, b):
                 check[i][j] = check[i-1][j-1] + 1
             else:
                 check[i][j] = max(check[i-1][j],check[i][j-1])
-            
             res = max(check[i][j], res)
-    
     return res
 
 a = ""
--- test.py
+++ test.py
@@ -1,31 +1,4 @@
-# adjust a array
-# problem in lintcode
-
-
-def MinAdjustmentCost(self, nums, target):
-    # write your code here
-    # classical dp problem
-    # first find the max value in the array, so each figure should not exceed this range
-    # so define a matrix d
-    # d[i][j] represents the min different, if nums[i] change to j(j<max_val)
-    # it depends on the d[i-1][p], but for p should in (j-target,j+target)
-    # then for the last integer, we need to get the result from the last
-    
-    # 
-    int_max = 2**31-1
-    max_val = max(nums)
-    d = [[0 for i in range(max_val+1)] for j in range(len(nums))]
-    for j in range(max_val+1):
-        d[0][j] = abs(nums[0]-j)
-        
-    res = int_max
-    for i in range(len(nums)):
-        for j in range(max_val+1):
-            d[i][j] = int_max
-            for k in range(max(0,j-target),min(j+target+1,max_val+1)):
-                d[i][j] = min(d[i][j],d[i-1][k])
-            d[i][j] += abs(nums[i]-j)
-            if i == len(nums)-1:
-                res = min(res,d[i][j])
-    
-    return res
+def isInterleave(self, s1, s2, s3):
+        # write your code here
+        # Classical dp solution 
+        return True
